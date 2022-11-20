f3c61a2cc1f316a26e2e2d14d4fb885adf042797
Siddhant <siddhant@fastmail.com>
2022-11-20 02:34:28 +0100

    til: terminal-colors

--- app/routes/t/_data.tsx
+++ app/routes/t/_data.tsx
@@ -2,7 +2,8 @@ import { mdxToPosts } from "~/util/mdxToPosts";
 import { sort } from "~/util/sort";
 import * as cryptoSignatures from "./crypto-signatures.mdx";
 import * as sockets from "./sockets.mdx";
+import * as terminalColors from "./terminal-colors.mdx";
 
 export function data() {
-  return sort(mdxToPosts([cryptoSignatures, sockets], "til"));
+  return sort(mdxToPosts([cryptoSignatures, sockets, terminalColors], "til"));
 }
