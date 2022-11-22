5c62d9f14cd92c145af640924c1c189942215c95
Siddhant <siddhant@fastmail.com>
2022-11-22 02:20:30 +0100

    fix: responsiveness

--- app/components/Prose.tsx
+++ app/components/Prose.tsx
@@ -1,27 +1,22 @@
-import { useMatches } from "@remix-run/react";
 import clsx from "clsx";
 import { PropsWithChildren } from "react";
 
 type Props = PropsWithChildren<{}>;
 export function Prose({ children }: Props) {
-  const matches = useMatches();
-  const isRoot =
-    matches[matches.length - 1].pathname === "/til/" ||
-    matches[matches.length - 1].pathname === "/posts/";
-
   return (
     <section
-      className={clsx({
-        "prose prose-default": !isRoot,
-        "prose-h1:font-mono prose-h1:font-medium": !isRoot,
-        "prose-h2:font-mono prose-h2:font-medium": !isRoot,
-        "prose-h3:font-mono prose-h3:font-medium": !isRoot,
-        "prose-h4:font-mono prose-h4:font-medium": !isRoot,
-        "prose-h5:font-mono prose-h5:font-medium": !isRoot,
-        "prose-h6:font-mono prose-h6:font-medium": !isRoot,
-        "prose-pre:p-0": !isRoot,
-        "prose-a:break-all": !isRoot,
-      })}
+      className={clsx(
+        "prose prose-default",
+        "prose-h1:font-mono prose-h1:font-medium",
+        "prose-h2:font-mono prose-h2:font-medium",
+        "prose-h3:font-mono prose-h3:font-medium",
+        "prose-h4:font-mono prose-h4:font-medium",
+        "prose-h5:font-mono prose-h5:font-medium",
+        "prose-h6:font-mono prose-h6:font-medium",
+        "prose-pre:p-0 prose-pre:whitespace-pre-wrap",
+        "prose-a:break-all",
+        "prose-img:w-fit prose-img:mx-auto"
+      )}
     >
       {children}
     </section>
