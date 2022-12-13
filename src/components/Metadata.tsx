e20c99de19de0f85b1be61f6251236884e5ee6c9
Siddhant <siddhant@fastmail.com>
2022-12-13 04:03:32 +0100

    chore: post-level meta tags

--- src/components/Metadata.tsx
+++ src/components/Metadata.tsx
@@ -2,10 +2,52 @@ import { For } from "solid-js";
 import { Title } from "solid-start";
 import { Post } from "./PostList";
 
-export function Metadata({ title, date, tags = [] }: Post) {
+type MetadataProps = {
+  title: Post["title"];
+  description: Post["description"];
+  date: Post["date"];
+  tags: Post["tags"];
+  url: string;
+};
+
+export function Metadata({
+  title,
+  description,
+  date,
+  tags = [],
+  url,
+}: MetadataProps) {
+  // const slugPrefix = kind === "post" ? "p" : "t";
+  // console.log({ kind, slug });
+
+  console.log({ url });
   return (
     <>
+      {url}
       <Title>{title}</Title>
+      {/* <Meta property="og:type" content="website" />
+      <Meta
+        property="og:url"
+        content={`https://dietcode.io/${slugPrefix}/${slug}`}
+      />
+      <Meta property="og:title" content={title} />
+      <Meta
+        property="og:description"
+        content={description ?? "Sid's adventures with computers"}
+      />
+      <Meta property="og:image" content="https://dietcode.io/meta.png" />
+      <Meta property="twitter:card" content="summary_large_image" />
+      <Meta
+        property="twitter:url"
+        content={`https://dietcode.io/${slugPrefix}/${slug}`}
+      />
+      <Meta property="twitter:title" content={title} />
+      <Meta
+        property="twitter:description"
+        content={description ?? "Sid's adventures with computers"}
+      />
+      <Meta property="twitter:image" content="https://dietcode.io/meta.png" /> */}
+
       <div class="flex items-center justify-between text-mauve-11 h-12">
         <div>{date}</div>
 
