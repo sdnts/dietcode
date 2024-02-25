dddc5bc9b90198ca85dbf154aa9d5d18a78cd46a
Siddhant <git@sdnts.dev>
2024-02-25 14:10:55 -0600

    chore: date formats

--- src/util/formatDate.ts
+++ src/util/formatDate.ts
@@ -1,16 +1,31 @@
-export const formatDate = (date: Date) => {
-	const year = new Intl.DateTimeFormat("en-US", {
-		year: "numeric",
-		timeZone: "CET",
-	}).format(date);
-	const month = new Intl.DateTimeFormat("en-US", {
-		month: "2-digit",
-		timeZone: "CET",
-	}).format(date);
-	const day = new Intl.DateTimeFormat("en-US", {
-		day: "2-digit",
-		timeZone: "CET",
-	}).format(date);
+type Format = "short" | "long";
 
-	return `${year}-${month}-${day}`;
+export const formatDate = (date: Date, format: Format = "short") => {
+  if (format === "short") {
+    const year = new Intl.DateTimeFormat("en-US", {
+      year: "numeric",
+      timeZone: "CET",
+    }).format(date);
+    const month = new Intl.DateTimeFormat("en-US", {
+      month: "short",
+      timeZone: "CET",
+    }).format(date);
+
+    return `${month} ${year}`;
+  }
+
+  const year = new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    timeZone: "CET",
+  }).format(date);
+  const month = new Intl.DateTimeFormat("en-US", {
+    month: "long",
+    timeZone: "CET",
+  }).format(date);
+  const day = new Intl.DateTimeFormat("en-US", {
+    day: "numeric",
+    timeZone: "CET",
+  }).format(date);
+
+  return `${day} ${month} ${year}`;
 };
